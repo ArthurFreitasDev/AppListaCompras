233d2e330939a6b19dfb8ef56fd6a654375d600b
Usuario <Usuario@CPD03-M16>
2024-03-14 14:20:32 -0300

    Commit

--- AppListaCompras/MainPage.xaml.cs
+++ AppListaCompras/MainPage.xaml.cs
@@ -1,24 +1,45 @@
-﻿namespace AppListaCompras
+﻿
+using System.Collections.ObjectModel;
+using AppListaCompras.Models;
+
+namespace AppListaCompras
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        ObservableCollection<Produto> Lista_Produtos = new ObservableCollection<Produto>();
 
         public MainPage()
         {
             InitializeComponent();
+            list_produtos.ItemsSource = Lista_Produtos;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
         {
-            count++;
+            double Soma = Lista_Produtos.Sum(i => (i.Preco * i.Quantidade));
+            string msg = $"O total é {Soma.ToString("C")}";
+            DisplayAlert("Somatória", msg, "Fechar");
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void ToolbarItem_Clicked_Adicionar(object sender, EventArgs e)
+        {
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        protected override void OnAppearing()
+        {
+            if(Lista_Produtos.Count == 0)
+            {
+                Task.Run(async () =>
+                {
+                    List<Produto> tmp = await App.Db.GetAll();
+                    foreach(Produto p in tmp)
+                    {
+                        Lista_Produtos.Add(p);
+                    }
+                });
+            }
         }
     }
 
